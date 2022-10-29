0cccf4b86b56a3003d0d128e518e8b3a2544b038
jose <jrodriguez@itsolutionspcllc.com>
2022-10-28 23:27:14 -0400

    Classes-Exercise

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace Classes
+﻿using System.Collections.Generic;
+using System;
+namespace Classes
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var honda = new Car();
+            honda.Make =  "HONDA";
+            honda.Model = "Civic";
+            honda.Year =    2023;
+
+            var bwm = new Car();
+            bwm.Make =     "BWM";
+            bwm.Model =    "MP4";
+            bwm.Year =      2022;
+            
+
+
+            var carList = new List<Car>();
+            carList.Add(honda);
+            carList.Add(bwm);
+
+            Console.WriteLine("Cars Inventory");
+            Console.WriteLine("******************************");
+            Console.WriteLine("Make    Model       Year");
+
+
+
+            foreach (var item in carList)
+            {
+
+              
+
+                Console.WriteLine($"  { item.Make}   { item.Model}   { item.Year}");
+            }
+            
         }
     }
 }
